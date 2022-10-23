67fdc389da35828b9cf1a33f26a574dc24011987
AntoineHuet49 <antoine.200@orange.fr>
2022-10-23 12:01:56 +0200

    Faute de frappe

--- app/views/home.php
+++ app/views/home.php
@@ -2,6 +2,6 @@
 <div class="main">
   <div class="name"><h1 class="name__title">Huet Antoine</h1></div>
   <div class="séparation"></div>
-  <div class="job"><p>Développeur Web et Web Movile</p></div>
+  <div class="job"><p>Développeur Web et Web Mobile</p></div>
 </div>
 </main>
